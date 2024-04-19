69792dc00d8837b6e59e2bb3f9ba1794e53632f3
Ron Buening <ron@ronbuening.com>
2024-04-19 11:16:11 -0400

    try & catch exception handling implemented

--- WEEK1/fridayProgram/Program.cs
+++ WEEK1/fridayProgram/Program.cs
@@ -4,12 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please key value 1 as int");
-        int val1 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Please key value 2 as int");
-        int val2 = Convert.ToInt32(Console.ReadLine());
-        //Below uses string interpolation to insert the values and answer. Note the '$' at the beginning of the string.
-        Console.WriteLine($"The sum of {val1} and {val2} is {AddTwoNumbers(val1,val2)}");
+        int val1;
+        int val2;
+        bool test1 = false;
+        
+        do 
+        {
+            try //code we are QCing to ensure there's not a crash
+            {
+                Console.WriteLine("Please key value 1");
+                val1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please key value 2");
+                val2 = Convert.ToInt32(Console.ReadLine());
+                //Below uses string interpolation to insert the values and answer. Note the '$' at the beginning of the string.
+                Console.WriteLine($"The sum of {val1} and {val2} is {AddTwoNumbers(val1,val2)}");
+                test1 = true;
+            }
+            catch (Exception e) //doing something when there's a potential exception; there may be multiple catch statements based on the needs of the program and type of exception
+            {
+                //This sort of statement will print the exception's message to the user
+                Console.WriteLine($"{e.Message} Please enter an integer value.");
+                //Console.WriteLine($"{e.StackTrace} is the error location");
+            }
+           //finally //will always execute after the first two commands
+            //{
+
+            //}
+        }
+        while (test1 == false);
+        
+       
     }
 
     //static methods vs dynamic methods will be discussed next week; these are access modifiers
@@ -19,4 +43,19 @@ class Program
     {
         return num1 + num2;
     }
+
+    //exception handling
+    //sometimes people do stupid things
+    //computers are rigid and not bright
+    //we have to account for both
+    //this one is a little broke
+    /*
+    static string NullCatch (string nullTest)
+    {
+        if (nullTest == null)
+            throw new invalidNull;
+        else 
+            return nullTest;
+    }
+    */
 }
