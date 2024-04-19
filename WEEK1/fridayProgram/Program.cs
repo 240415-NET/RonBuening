b9c47c22f67ab0a97fd399f398fbd0a80b170836
Ron Buening <ron@ronbuening.com>
2024-04-19 11:30:40 -0400

    added additional comments regarding assignment of values, exception handling and ux

--- WEEK1/fridayProgram/Program.cs
+++ WEEK1/fridayProgram/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        int val1;
-        int val2;
+        int val1 = 0; //values need to be assigned to val1 and val2 to ensure that a value is present for line 36 outside the do-while/try-catch blocks
+        int val2 = 0; //this is caught by the compiler and it does not recognize the values _will_ get assigned in the do-while loop
         bool test1 = false;
         
+        //having a single do-while loop will require the user to key both values again if val2 is invalid
         do 
         {
             try //code we are QCing to ensure there's not a crash
@@ -16,8 +17,9 @@ class Program
                 val1 = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Please key value 2");
                 val2 = Convert.ToInt32(Console.ReadLine());
-                //Below uses string interpolation to insert the values and answer. Note the '$' at the beginning of the string.
-                Console.WriteLine($"The sum of {val1} and {val2} is {AddTwoNumbers(val1,val2)}");
+                //Below uses string interpolation to insert the values and answer. Note the '$' at the beginning of the string. 
+                //Placement here does not require the assignment of values at the start of the code
+                //Console.WriteLine($"The sum of {val1} and {val2} is {AddTwoNumbers(val1,val2)}");
                 test1 = true;
             }
             catch (Exception e) //doing something when there's a potential exception; there may be multiple catch statements based on the needs of the program and type of exception
@@ -33,6 +35,9 @@ class Program
         }
         while (test1 == false);
         
+        //Below uses string interpolation to insert the values and answer. Note the '$' at the beginning of the string. 
+        //Placement here requires the assignment of values at the start of the code
+        Console.WriteLine($"The sum of {val1} and {val2} is {AddTwoNumbers(val1,val2)}");
        
     }
 
