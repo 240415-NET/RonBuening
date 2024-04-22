f5fad2034c1f6cff9a4740e9307613dba4eb494c
Ron Buening <ron@ronbuening.com>
2024-04-22 13:21:54 -0400

    fully functional, but lacks error handling

--- Week2Team2Hackathon/Program.cs
+++ Week2Team2Hackathon/Program.cs
@@ -20,9 +20,10 @@ class Program
         string userInput = Console.ReadLine().Trim();
         bool quit = false;
         int itemNum = 1;
+        int alterItem;
         List<string> shoppingList = new List<string>();
-        
-        if (userInput.ToLower() == "q")
+
+        if (userInput.ToLower() == "q" || userInput.ToLower() == "quit")
         {
             quit = true;
         }
@@ -37,13 +38,31 @@ class Program
         {
             Console.WriteLine("Please enter an item for your shopping list or type 'print' to print list");
             userInput = Console.ReadLine().Trim();
-            if (userInput.ToLower() == "q")
+            if (userInput.ToLower() == "q" || userInput.ToLower() == "quit")
             {
                 quit = true;
             }
-            else if (userInput.ToLower() == "print")
+            else if (userInput.ToLower() == "p" || userInput.ToLower() == "print")
             {
+                Console.Clear();
                 shoppingList.ForEach(Console.WriteLine);
+                Console.WriteLine("Do you need to add more items or change an existing item?");
+                Console.WriteLine("'y' for yes, 'n' for no, 'c' to change");
+                userInput = Console.ReadLine().Trim().ToLower();
+                if (userInput == "n" || userInput == "q" || userInput == "no" || userInput == "quit")
+                {
+                    quit = true;
+                }
+                else if (userInput == "c" || userInput == "change")
+                {
+                    Console.WriteLine("Please enter an index to change");
+                    alterItem = Convert.ToInt32(Console.ReadLine().Trim());
+                    Console.WriteLine("What do you want to replace " + shoppingList[alterItem-1] + " with?");
+                    userInput = Console.ReadLine().Trim();
+                    shoppingList[alterItem-1] = alterItem + ". " + userInput;
+                    Console.Clear();
+                    shoppingList.ForEach(Console.WriteLine);
+                }
             }
             else
             {
