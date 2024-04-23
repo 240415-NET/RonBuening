a4c619e68d6baece7811fa1905f88c8e746642f8
Ron Buening <ron@ronbuening.com>
2024-04-23 12:53:06 -0400

    Beginning object creation

---
+++ Week2Team2Hackathon/ShopObjects.cs
@@ -0,0 +1,11 @@
+namespace Week2Team2Hackathon;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class ShopObjects
+{
+    public List<string> shopList = new();
+    public Dictionary<string,string> shopList = new();
+    
+}
