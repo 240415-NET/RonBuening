0330b88fa997694e8f2bfa283dce24158cfadcde
Ron Buening <ron@ronbuening.com>
2024-04-24 11:00:48 -0400

    Updating notes

--- WEEK2/ClassBasics/Program.cs
+++ WEEK2/ClassBasics/Program.cs
@@ -13,13 +13,34 @@ namespace ClassBasics
             
 
             // exampleClass.Name = "    Hello   "; // this will not compile as we are accessing a private variable
-
+/*
             exampleClass.SetName("   Hello         ");
             Console.WriteLine(exampleClass.GetName());
 
             exampleClass.Age = 55; // this is fine because the Age field is public
 
             Console.WriteLine(exampleClass.GetMaxLimit());
+*/
+            Person personA = new Person();
+
+            Console.WriteLine(personA.GetFirstName());
+            Console.WriteLine(personA.GetLastName());
+            Console.WriteLine(personA.GetEmail());
+            Console.WriteLine(personA.GetAge());
+            Console.WriteLine(personA.GetOnHoliday);
+
+            Person personB = new Person();
+            personB.SetFirstName("Brian");
+            personB.SetLastName("Arayathel");
+            personB.SetEmail("brian.arayathe@revature.com");
+            personB.SetAge(25);
+            personB.SetOnHoliday(false);
+
+            Console.WriteLine(personB.GetFirstName());
+            Console.WriteLine(personB.GetLastName());
+            Console.WriteLine(personB.GetEmail());
+            Console.WriteLine(personB.GetAge());
+            Console.WriteLine(personB.GetOnHoliday);
         }
     }
 
