d55780f7b82593b43bd433fcbf49ce7b575e07cf
Ron Buening <ron@ronbuening.com>
2024-04-24 15:19:03 -0400

    dictionary kind of working

--- Week2Team2Hackathon/StockTake.cs
+++ Week2Team2Hackathon/StockTake.cs
@@ -21,7 +21,7 @@ class StockTake// : IENumerable<ShopObjects>
         
 
         bool quit = false;
-        bool done = false;
+        bool done1 = false;
         int itemNumL = 1;
         int aisleL;
         int stockL;
@@ -42,8 +42,8 @@ class StockTake// : IENumerable<ShopObjects>
                 Console.WriteLine("When inventory is done, enter 'd' for done");
                 Console.WriteLine("Please enter the product name");
                 buffer = Console.ReadLine().Trim();
-                done = doneChecker(buffer);
-                if (done == false)
+                done1 = doneChecker(buffer);
+                if (done1 == false)
                 {
                     productNameL = Program.exitChecker(buffer);
                     Console.WriteLine("Please enter the brand name");
@@ -60,7 +60,19 @@ class StockTake// : IENumerable<ShopObjects>
                     {
                         localInventory[i].WriteStock();
                     }
-                    quit = true;
+                    Console.WriteLine("Do you need to add additional items?");
+                    Console.WriteLine("'y' for yes, 'n' for no, 'c' to change existing item");
+                   /* buffer = Console.ReadLine.Trim();
+                    if (buffer.ToLower == "no" || buffer.ToLower == "n")
+                    {
+                        Console.WriteLine("Ready to save");
+                        FileHandling.SaveInventory(saveInventory);
+                        quit = true;
+                    }
+                    else
+                    {
+                        localInventory = editNeeded(localInventory);
+                    }*/
                 }
             }
             catch (Exception s)
@@ -72,11 +84,51 @@ class StockTake// : IENumerable<ShopObjects>
 
     private static bool doneChecker(string doneCheck)
     {
-        bool done = false;
+        bool doneTest = false;
         if (doneCheck.ToLower() == "d" || doneCheck.ToLower() == "done")
         {
-            done = true;
+            doneTest = true;
         }
-        return done;
+        return doneTest;
     }
+/*
+    private static Dictionary<int,<ShopObjects>> editNeeded(string editCheck,Dictionary<int,<ShopObjects>> printableInventory)
+    {
+        bool editResult = false;
+        Dictionary<int,<ShopObjects>> saveInventory = printableInventory.ToDictionary(k => k.Key, k => k.Value.ToString());
+        if (editCheck == "q" || editCheck == "quit")
+        {
+            Program.exitChecker(editCheck);
+        }
+        else if (editCheck == "c" || editCheck == "change")
+        {
+            saveInventory = changeInventory(saveInventory);
+        }
+        else
+        {
+            Console.WriteLine("Please enter valid selection.");
+        }
+        return saveInventory;
+    }
+
+    private static Dictionary<int,<ShopObjects>> changeInventory(Dictionary<int,<ShopObjects>> original)
+    {
+        Dictionary<int,<ShopObjects>> originalInventory = original.ToDictionary(k => k.Key, k => k.Value.ToString());
+        int changeInv;
+        string changeInvString;
+        for (int i = 0; i < originalInventory.Count(); i++)
+        {
+            originalInventory[i].WriteStock();
+        }
+        Console.WriteLine("Please enter index to change");
+        try
+        {
+            changeInv = Convert.ToInt32(Console.ReadLine());
+        }
+        catch (Exception i)
+        {
+            Console.WriteLine("Please enter integer value");
+        }
+
+    }*/
 }
