6786cee0c9e063523729576f4d1caabc27b0b1a8
Ron Buening <ron@ronbuening.com>
2024-04-24 10:19:28 -0400

    adding classes notes

--- WEEK2/ClassBasics/Program.cs
+++ WEEK2/ClassBasics/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace ClassBasics;
+﻿﻿using ClassBasics.Classes;
 
-class Program
+namespace ClassBasics
 {
-    static void Main(string[] args)
+    public class ClassBasics
     {
-        Console.WriteLine("Hello, World!");
+        public static void Main(string[] args)
+        {
+            ExampleClass exampleClass = new ExampleClass {Age = 30};
+
+            // exampleClass.Name = "    Hello   "; // this will not compile as we are accessing a private variable
+
+            exampleClass.SetName("   Hello         ");
+            Console.WriteLine(exampleClass.GetName());
+
+            exampleClass.Age = 55; // this is fine because the Age field is public
+
+            Console.WriteLine(exampleClass.GetMaxLimit());
+        }
     }
+
 }
