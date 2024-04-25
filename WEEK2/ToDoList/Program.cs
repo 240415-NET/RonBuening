f5011b954d8e06d04688de915d1c455b9913905c
Ron Buening <ron@ronbuening.com>
2024-04-25 09:07:47 -0400

    resyncing the example program

--- WEEK2/ToDoList/Program.cs
+++ WEEK2/ToDoList/Program.cs
@@ -10,5 +10,14 @@ class Program
         Console.WriteLine(toDo);
         ToDoItem toDo2 = new ToDoItem("Get milk",60,"2024-04-25",false);
         Console.WriteLine(toDo2);
+        Console.Clear();
+        Console.WriteLine("Type Description:");
+        string description = Console.ReadLine();
+        Console.WriteLine("Estimated Time:");
+        int estimatedTime = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Due Date:");
+        string dueDate = Console.ReadLine();
+        ToDoItem toDo3 = new ToDoItem(description,estimatedTime,dueDate,false);
+        Console.WriteLine(toDo3);
     }
 }
