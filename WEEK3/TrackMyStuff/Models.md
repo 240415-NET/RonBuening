c47f216a8984129e004a1e4422a9b91b6eecc40e
Ron Buening <ron@ronbuening.com>
2024-04-30 11:26:14 -0400

    Moving model description to new .md file

---
+++ WEEK3/TrackMyStuff/Models.md
@@ -0,0 +1,17 @@
+    ## Models
+    - User
+        - userID (int)
+    - Common fields (Item Class)
+        - itemID (int/GUID)
+        - Owner (int)
+        - Item Category (string)
+        - Original cost (double)
+        - Purchase date (DateTime)
+        - Description (string)
+    - Pets (extending Item)
+        - Name (string)
+        - Species (string)
+        - Age (int)
+    - Documents (extending Item)
+        - Document Type (string)
+        - Expiration (DateTime)
--- WEEK3/TrackMyStuff/Program.cs
+++ WEEK3/TrackMyStuff/Program.cs
@@ -1,25 +1,7 @@
 ﻿namespace TrackMyStuff.App;
 
-/*
-    ##Models
-    -User
-        -userID (int)
-    -Common fields
-        -Owner (int)
-        -Item Category (string)
-        -Original cost (float)
-        -Purchase date (date)
-        -Description (string)
-    -Pets
-        -Name (string)
-        -Species (string)
-        -Age (string)
-    -Documents
-        -Document Type (string)
-        -Expiration (date)
 
 
-*/
 class Program
 {
     static void Main(string[] args)
