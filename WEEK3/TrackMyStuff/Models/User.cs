0210523048034a3970cd5ccf5c3d579d446f3188
Ron Buening <ron@ronbuening.com>
2024-04-30 12:28:00 -0400

    passwords

--- WEEK3/TrackMyStuff/Models/User.cs
+++ WEEK3/TrackMyStuff/Models/User.cs
@@ -7,6 +7,7 @@ public class User
     //You can leverage get; set; shorthand to protect fields by putting a different access modified to the getter or setter
     public int userId {get; private set;}
     public string userName {get; set;}
+    private string password {get; set;}
 
     //Constructors
     public User() {}
@@ -14,6 +15,12 @@ public class User
     {
         userId = _userId;
         userName = _userName;
+    }
+    public User (int _userId, string _userName, string _password)
+    {
+        userId = _userId;
+        userName = _userName;
+        password = _password;
     }
 
 }
