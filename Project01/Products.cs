832773ed5a24e7de6529340411ce4c04b102ab1b
Ron Buening <ron@ronbuening.com>
2024-04-30 12:52:20 -0400

    More constructors

--- Project01/Products.cs
+++ Project01/Products.cs
@@ -37,12 +37,20 @@ class Camera : Product
     public float resolution {get; set;}
     public bool interchangeableLens {get; set;}
     public string lensMountReceive {get; set;}
-    public bool viewfinder {get; set;}
-    public float screenResolution {get; set;}
+    public bool? viewfinder {get; set;}
+    public float? screenResolution {get; set;}
     public Camera () {}
     public Camera (int itemID, string dept, string brandName, string productName, float resolution, bool interchangeableLens, string lensMountReceive, bool viewfinder, float screenResolution) :base Product (itemID, dept, brandName, productName)
     {
-
+        this.itemID = itemID;
+        this.dept = "Camera";
+        this.brandName = brandName;
+        this.productName = productName;
+        this.resolution = resolution;
+        this.interchangeableLens = interchangeableLens;
+        this.lensMountReceive = lensMountReceive;
+        this.viewfinder = viewfinder;
+        this.screenResolution = screenResolution;
     }
 
 }
@@ -52,13 +60,22 @@ class Lens : Product
     public bool zoom {get; set;}
     public int minFocalLength {get; set;}
     public int maxFocalLength {get; set;}
-    public int minAperture {get; set;}
-    public int maxAperture {get; set;}
+    public int? minAperture {get; set;}
+    public int? maxAperture {get; set;}
     public string lensMount {get; set;}
     public Lens () {}
     public Lens (int itemID, string dept, string brandName, string productName, bool zoom, int minFocalLength, int maxFocalLength, int minAperture, int maxAperture, string lensMount) :base Product (itemID, dept, brandName, productName)
     {
-
+        this.itemID = itemID;
+        this.dept = "Lens";
+        this.brandName = brandName;
+        this.productName = productName;
+        this.zoom = zoom;
+        this.minFocalLength = minFocalLength;
+        this.maxFocalLength = maxFocalLength;
+        this.minAperture = minAperture;
+        this.maxAperture = maxAperture;
+        this.lensMount = lensMount;
     }
 }
 
@@ -66,24 +83,37 @@ class Lighting : Product
 {
     public bool wallPower {get; set;}
     public bool flash {get; set;}
-    public int wattSecond {get; set;}
-    public int guideNumber {get; set;}
-    public string modifierMountReceive {get; set;}
+    public int? wattSecond {get; set;}
+    public string? modifierMountReceive {get; set;}
     public Lighting () {}
-    public Lighting (int itemID, string dept, string brandName, string productName, bool wallPower, bool flash, int wattSecond, int guideNumber, string modifierMountReceive) :base Product (itemID, dept, brandName, productName)
+    public Lighting (int itemID, string dept, string brandName, string productName, bool wallPower, bool flash, int wattSecond, string modifierMountReceive) :base Product (itemID, dept, brandName, productName)
     {
-
+        this.itemID = itemID;
+        this.dept = "Lighting";
+        this.brandName = brandName;
+        this.productName = productName;
+        this.wallPower = wallPower;
+        this.flash = flash;
+        this.wattSecond = wattSecond;
+        this.modifierMountReceive = modifierMountReceive;
     }
 }
 class Modifiers : Product
 {
     public string modifierType {get; set;}
     public string modifierMount {get; set;}
-    public int modifierHeight {get; set;}
-    public int modifierWidth {get; set;}
+    public int? modifierHeight {get; set;}
+    public int? modifierWidth {get; set;}
     public Modifiers () {}
     public Modifiers (int itemID, string dept, string brandName, string productName, string modifierType, string modifierMount, int modifierHeight, int modifierWidth) :base Product (itemID, dept, brandName, productName)
     {
-
+        this.itemID = itemID;
+        this.dept = "Modifiers";
+        this.brandName = brandName;
+        this.productName = productName;
+        this.modifierType = modifierType;
+        this.modifierMount = modifierMount;
+        this.modifierHeight = modifierHeight;
+        this.modifierWidth = modifierWidth;
     }
 }
