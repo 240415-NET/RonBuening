bcae152813704b14d81e5c0d20c510fc1b903dec
Ron Buening <ron@ronbuening.com>
2024-05-03 11:43:12 -0400

    Prevent duplicate user creation based on email

--- cSharpBird/Presentation/UserCreation.cs
+++ cSharpBird/Presentation/UserCreation.cs
@@ -17,18 +17,19 @@ public class UserCreation
             if (String.IsNullOrEmpty(email))
             {
                 Console.Clear();
-                UserInterface.WriteColors("{=Green}Email{/} cannot be blank. Please try again");
+                UserInterface.WriteColors("{=Green}Email{/} cannot be blank. Please try again\n");
             }
             else if(UserCreation.UserDupe(email))
             {
                 exitLoop = true;
-                UserInterface.WriteColors("{=Green}Email{/} already in use. Please sign in");
+                UserInterface.WriteColors("{=Green}Email{/} already in use. Please sign in\n");
                 UserMaintenance.LogIn();
             }
             else
             {
                 exitLoop = true;
                 User currentSession = NewUser(email);
+                UserInterface.WriteColors("{=Blue}New user created for " + email +"\n");
                 UserMaintenance.UserMenu(currentSession);
             }
         }
@@ -36,7 +37,16 @@ public class UserCreation
     }
     public static bool UserDupe(string userRequested)
     {
-        return false;
+        string email = userRequested;
+        User foundUser = new User();
+        //This method is used to find the user attempting to sign in and return the proper user object
+        //Will return full user list
+        List<User> userList = AccessFile.ReadUser();
+        foundUser = userList.FirstOrDefault(u => u.userName.ToLower() == email.ToLower());
+        if (foundUser == null)
+            return false;
+        else
+            return true;
     }
     public static User NewUser(string email)
     {
