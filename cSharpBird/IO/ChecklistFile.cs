59c582e2000f1d9ace9cd9d849ad5a7a75a2f37f
Ron Buening <ron@ronbuening.com>
2024-05-06 15:27:15 -0400

    Completed(?) WriteChecklist method

--- cSharpBird/IO/ChecklistFile.cs
+++ cSharpBird/IO/ChecklistFile.cs
@@ -24,9 +24,46 @@ public class ChecklistFile
             existingChecklistJSON = JsonSerializer.Serialize(ChecklistArchive);
             File.WriteAllText(pathFile,existingChecklistJSON);
         }
-       // IEnumerable<Checklist> temp = ChecklistArchive.Where(i => i.userId == searchUser.userId);
+        //IEnumerable<Checklist> temp = ChecklistArchive.Where(i => i.userId == searchUser.userId);
         //userChecklists = temp.ToList();
         userChecklists = ChecklistArchive.Where(i => i.userId == searchUser.userId).ToList();
         return userChecklists;
+    }
+    public static void WriteChecklist(Checklist newList)
+    {
+        //This method will write a new Checklist to the json file, creating it if it does not exist.
+        string pathFile = "Checklists.json"";
+        List<Checklist> ChecklistArchive = new List<Checklist>();
+        string existingChecklistJSON;
+        if (File.Exists(pathFile))
+        {
+            existingChecklistJSON =File.ReadAllText(pathFile);
+            ChecklistArchive = JsonSerializer.Deserialize<List<Checklist>>(existingChecklistJSON);
+            ChecklistArchive.Add(newList);
+            existingChecklistJSON = JsonSerializer.Serialize(ChecklistArchive);
+            File.WriteAllText(pathFile,existingChecklistJSON);
+        }
+        else if(!File.Exists(pathFile))
+        {
+            ChecklistArchive.Add(new Checklist(searchUser.userId,"defaultLocation"));
+            existingChecklistJSON = JsonSerializer.Serialize(ChecklistArchive);
+            File.WriteAllText(pathFile,existingChecklistJSON);
+        }
+    }
+    public static void WriteUpdatedList(Checklist updatedList)
+    {
+        //This method will write a new user to the json file, creating it if it does not exist.
+        List<Checklist> ChecklistArchive = new List<Checklist>();
+        string existingChecklistJSON;
+
+        existingChecklistJSON =File.ReadAllText(pathFile);
+        ChecklistArchive = JsonSerializer.Deserialize<List<User>>(existingChecklistJSON);
+        User oldUser = ChecklistArchive.First(i => i.userId == updatedUser.userId);
+        var userLocation = ChecklistArchive.IndexOf(oldUser);
+        if (userLocation != -1)
+            ChecklistArchive[userLocation] = updatedUser;
+        
+        existingChecklistJSON = JsonSerializer.Serialize(ChecklistArchive);
+        File.WriteAllText(pathFile,existingChecklistJSON);
     }
 }
