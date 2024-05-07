d78b40ee3d0fdb0074852d8b8ca82c62b9bd73d2
Ron Buening <ron@ronbuening.com>
2024-05-07 09:40:53 -0400

    Revised Checklist class

--- cSharpBird/Objects/Checklist.cs
+++ cSharpBird/Objects/Checklist.cs
@@ -8,7 +8,7 @@ public class Checklist
     public Guid userId {get;set;}
     public string locationName {get; set;}
     public DateTime checklistDateTime {get; set;}
-    public List<Bird>? birdChecklist {get; set;}
+    public Dictionary<int,Bird>? birdChecklist {get; set;}
     //public string birder {get; set;}
     //public List<string> birds {get;set;}
     public Checklist() {}
@@ -18,8 +18,8 @@ public class Checklist
         checklistID = Guid.NewGuid();
         userId = _userId;
         locationName = _locationName;
-        checklistDateTime = DateTime.Today;
-        List<Bird> birdChecklist = new List<Bird>();
+        checkDictionaryDateTime = DateTime.Today;
+        Dictionary<int,Bird> birdChecklist = new Dictionary<int,Bird>();
         //List<string> birds = null;
     }
     public Checklist(Guid _userId, string _locationName, string _checklistDateTime)
@@ -28,7 +28,7 @@ public class Checklist
         userId = _userId;
         locationName = _locationName;
         checklistDateTime = DateTime.Parse(_checklistDateTime);
-        List<Bird> birdChecklist = new List<Bird>();
+        Dictionary<int,Bird> birdChecklist = new Dictionary<int,Bird>();
         //List<string> birds = null;
     }
 }
