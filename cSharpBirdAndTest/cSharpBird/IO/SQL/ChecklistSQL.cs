537bb938a85ea49f194fd52cb846c9913c8795c2
Ron Buening <ron@ronbuening.com>
2024-05-20 08:52:07 -0400

    Working User SQL, but may need changes to Checklist structure to normalize and work better -- will need to align JSON version with this.

--- cSharpBirdAndTest/cSharpBird/IO/SQL/ChecklistSQL.cs
+++ cSharpBirdAndTest/cSharpBird/IO/SQL/ChecklistSQL.cs
@@ -5,12 +5,42 @@ using System.IO;
 using System.Data.SqlClient;
 
 public class ChecklistSQL //: IAccessChecklistFile
-{
-    /*
-    public List<Checklist> GetLists(User searchUser);
-    string cmdText = "SELECT checklistID, userId, locationName, checklistDateTime, birds, distance, duration, stationary, cNotes FROM checklists;";
-    public void WriteChecklist(Checklist newList);
+{/*
+    string _connectionstring = File.ReadAllText("C:\\Users\\U0LA19\\Documents\\cSharpBird_DataSource.txt");
+    
+    public List<Checklist> GetLists(User searchUser)
+    {
+        List<Checklist> userChecklists = new List<Checklist>();
 
-    public void WriteUpdatedList(Checklist updatedList);
-    public void DeleteChecklist(Checklist deleteChecklist);*/
+        using SqlConnection connection = new SqlConnection(_connectionstring);
+        connection.Open();
+
+        string cmdText = "SELECT checklistID, userId, locationName, checklistDateTime, birds, distance, duration, stationary, cNotes FROM checklists WHERE userId = @userId;";
+
+        using SqlCommand cmd = new SqlCommand(cmdText,connection);
+        cmd.Parameters.AddWithValue("@userId",searchUser.userId);
+
+        using SqlDataReader reader = cmd.ExecuteReader();
+     
+        while(reader.Read())
+        {
+            if (reader.GetString(1) != null)
+                userChecklists.Add(new Checklist(reader.GetGuid(0),reader.GetGuid(1),reader.GetString(2),reader.GetString(3)));
+        }
+        connection.Close();
+        return userArchive;
+    }
+    public void WriteChecklist(Checklist newList)
+    {
+
+    }
+
+    public void WriteUpdatedList(Checklist updatedList)
+    {
+
+    }
+    public void DeleteChecklist(Checklist deleteChecklist)
+    {
+
+    }*/
 }
